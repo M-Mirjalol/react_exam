aea113816987e320d02426436ffca8fe21e4cf5c
Sizning GitHub username <sizning_email@github.com>
2026-02-08 19:50:08 +0500

    Some changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Img1 from "./Images/rasm1.svg";
 import Img2 from "./Images/Rasm2.svg";
-import Img3 from "./Images/rasm3";
+import Img3 from "./Images/rasm3.svg";
 
 const Hero = () => {
   const stats = [
